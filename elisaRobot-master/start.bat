4034dc6f449453d8183d0571400b7d6813ce476c
Neha Agrawal <88786146+iamproaf@users.noreply.github.com>
2023-10-02 05:52:07 +0530

    Delete elisaRobot-master directory

--- elisaRobot-master/start.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-TITLE  Ōɖìղ • オーディン
-:: Enables virtual env mode and then starts the bot
-env\scripts\activate.bat && py -m tg_bot
--- elisaRobot-master/tg_bot/modules/lockdown.py
+++
@@ -1,7 +0,0 @@
-# from .language import gs
-
-# def get_help(chat):
-#     return gs(chat, "lockgroup_help")
-
-# __mod_name__ = "🛡️ Lockdown 🛡️"
-# help moved to admin module
